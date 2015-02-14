ab61c8f355b59ce72d102a886050c6594415f022
Clément Gamé <quotekio@gmail.com>
2015-02-14 01:46:59 +0100

    Changed doxgen setup to make it more eye-candy.

--- export_lsclient.sh
+++ export_lsclient.sh
@@ -4,6 +4,8 @@ if [ -d ./lsclient_export/src ]; then
   rm -rf ./lsclient_export/src
 fi
 
+#git clone https://github.com/Velron/doxygen-bootstrapped ./lsclient_export/doxy-bootstrap
+
 cp -r src ./lsclient_export/
 
 rm ./lsclient_export/src/igconnector*
@@ -11,3 +13,8 @@ rm ./lsclient_export/src/broker.*
 rm ./lsclient_export/src/brokerio.*
 rm -rf ./lsclient_export/src/rapidjson
 
+#generating html doc
+cd lsclient_export/doxygen
+doxygen lsclient.doxy
+cp ext/doxy-boot.js ../doc/html/
+cd ../..
